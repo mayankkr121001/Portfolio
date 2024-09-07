ecdc90703c22d854e3fe0cf1bf938759585e065d
mayankkr121001 <mayanksahu121001@gmail.com>
2024-09-07 20:33:28 +0530

    Change in education section

--- src/component/Education.jsx
+++ src/component/Education.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 import educationIcon from "../assets/educationIcon.png"
+import img10th from "../assets/10thImg.jpg"
+import img12th from "../assets/12thImg.jpg"
 import bcaImg from "../assets/bcaImg.jpg"
 
+
+const educationArray = [
+    {
+        qualification: "10th - Board",
+        year: "2017-2018",
+        grade: "Percentage - 87%",
+        institution: "DAV Public School Baniahir, Dhanbad",
+        image: img10th
+    },
+    {
+        qualification: "12th - Science",
+        year: "2019-2020",
+        grade: "Percentage - 91%",
+        institution: "Rajkamal Sarawati Vidya Mandir, Dhanbad",
+        image: img12th
+    },
+    {
+        qualification: "Bachelor of Computer Application",
+        year: "2021-2024",
+        grade: "GPA - Result not declared yet",
+        institution: "Guru Nanak College, Dhanbad",
+        image: bcaImg
+    },
+]
 
 function Education() {
     return (
@@ -11,16 +37,21 @@ function Education() {
                     <img className='w-7' src={educationIcon} alt="education icon" />
                     <h1 className='text-xl sm:text-2xl md:text-4xl font-bold'>Education</h1>
                 </div>
-                <div className='educationCard flex flex-col gap-5 sm:flex-row justify-between rounded bg-zinc-950 px-5 sm:px-10 py-10 my-12'>
-                    <div className='text-left'>
-                        <h2 className='text-xl text-[#fff000]'>Bachelor of Computer Application</h2>
-                        <h2 className='py-2'>2021-2024</h2>
-                        <h2>Guru Nanak College, Dhanbad</h2>
+                {educationArray.map((elm, index) => (
+                    <div className='educationCard flex flex-col gap-5 sm:flex-row justify-between rounded bg-zinc-950 px-5 sm:px-10 py-8 my-12'>
+                        <div className='text-left'>
+                            <h2 className='text-xl text-[#fff000]'>{elm.qualification}</h2>
+                            <h2 className='text-sm py-2'>{elm.year}</h2>
+                            <h2 className='text-sm pb-2'>{elm.grade}</h2>
+                            <h2>{elm.institution}</h2>
+                        </div>
+                        <div>
+                            <img className='h-[120px]' src={elm.image} alt="education image" />
+                        </div>
                     </div>
-                    <div>
-                        <img className='h-[120px]'  src={bcaImg} alt="ducation image" />
-                    </div>
-                </div>
+
+                ))}
+
             </div>
         </div>
     )
