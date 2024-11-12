f92ce217ea04efcc4f0f8dc71b6840307e844459
mayankkr121001 <mayanksahu121001@gmail.com>
2024-11-12 13:09:01 +0530

    skills sizes updated

--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -49,17 +49,17 @@ function Skills() {
 
     return (
         <div id='Skills' className='w-full mt-[30px] pt-[130px] font-nunito'>
-            <div  className='text-center w-[55%] mx-auto'>
+            <div  className='text-center w-[80%] 2xl:w-[60%] mx-auto'>
                 <div className='flex gap-2 md:gap-5 justify-center items-center'>
                     <img className='w-6' src={laptopIcon} alt="icon" />
                     <h1 className='text-xl sm:text-2xl md:text-3xl font-bold'>Skills</h1>
                 </div>
-                <div className='skillsDiv py-12'>
+                <div className='skillsDiv py-12 flex flex-wrap gap-5 sm:gap-7 justify-center'>
                     {
                         skillsArray.map((elm, index) => (
-                            <div key={index} className='skillCard bg-zinc-950 px-7 py-8 flex flex-col items-center rounded-md'>
-                                <img className='w-[50px]' src={elm.image} alt="html icon" />
-                                <p className='pt-5 text-md text-[#c5ba22] font-bold'>{elm.name}</p>
+                            <div key={index} className='skillCard w-[130px] sm:w-[180px] bg-zinc-950 px-7 py-8 flex flex-col items-center rounded-md'>
+                                <img className='w-[30px] sm:w-[50px]' src={elm.image} alt="html icon" />
+                                <p className='pt-5 text-sm sm:text-md text-[#c5ba22] font-bold'>{elm.name}</p>
                             </div>
 
                         ))
